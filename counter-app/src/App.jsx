3b1ce6b39682195fb1533bf8b0e60bd07bec0f1b
김장호 <rlawkdgh0406@naver.com>
2024-10-05 20:20:24 +0900

    feat: useEffect로 LifeCycle 제어

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,24 +1,44 @@
+import { useRef } from "react"
 import { useState, useEffect } from "react"
 import "./App.css"
 import Controller from "./components/Controller"
+import Even from "./components/Even"
 import Viewer from "./components/Viewer"
 
 function App() {
   const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    console.log(`count : ${count}`)
-  }, [count])
+  const isMount = useRef(false)
 
   const onCountChange = (value) => {
     setCount(count + value)
   }
 
+  // useEffect로 라이프 사이클 제어
+
+  // 1. Mount(탄생)
+  useEffect(() => {
+    console.log("mount")
+  }, []) // 빈 배열 전달
+
+  // 2. Update(리렌더링, 변화)
+  useEffect(() => {
+    // mount 이후 업데이트(리렌더링) 발생할 때 업데이트 표시
+    if (!isMount.current) {
+      isMount.current = true
+      return
+    }
+    console.log("update")
+  })
+
+  // 3. unMount(죽음)
+  // Even 참고
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onCountChange={onCountChange} />
---
+++ counter-app/src/components/Even.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+
+const Even = () => {
+  useEffect(() => {
+    return () => {
+      console.log("unmount")
+    }
+  }, [])
+  return <div>Even</div>
+}
+
+export default Even
