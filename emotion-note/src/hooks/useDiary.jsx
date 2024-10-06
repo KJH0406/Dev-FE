6eeb81b225ecdce7838ef5b622c1514231518aca
김장호 <rlawkdgh0406@naver.com>
2024-10-07 00:27:09 +0900

    refactor: 현재 일기 데이터 호출 함수 Custom hook으로 변환

---
+++ emotion-note/src/hooks/useDiary.jsx
@@ -0,0 +1,25 @@
+import { useContext, useEffect, useState } from "react"
+import { DiaryStateContext } from "../App"
+
+// 현재 일기 데이터 불러오기
+const useDiary = () => {
+  const nav = useNavigate()
+
+  const data = useContext(DiaryStateContext)
+
+  const [currentDiaryItem, setCurrentDiaryItem] = useState()
+
+  useEffect(() => {
+    const currentDiaryItem = data.find((item) => String(item.id) === String(id))
+    if (!currentDiaryItem) {
+      const alertMessage = "존재하지 않는 페이지입니다."
+      window.alert(alertMessage)
+      nav("/", { replace: true })
+    }
+
+    setCurrentDiaryItem(currentDiaryItem)
+  }, [id, data])
+  return currentDiaryItem
+}
+
+export default useDiary
