aa2cacc9d5296fefa45936e6b4fbbd3f80578c9e
김장호 <rlawkdgh0406@naver.com>
2024-10-06 17:57:14 +0900

    feat: Diary CRUD 구현 및 context 상태관리 세팅(state, dispatch)

--- emotion-note/src/App.jsx
+++ emotion-note/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom"
+import { v4 as uuidv4 } from "uuid"
 
 // pages
 import Home from "./pages/Home"
@@ -10,6 +11,7 @@ import Notfound from "./pages/Notfound"
 // css
 import "./App.css"
 import { useReducer } from "react"
+import { createContext } from "react"
 
 const dummyData = [
   {
@@ -26,22 +28,99 @@ const dummyData = [
   },
 ]
 
+const CREATE = "CREATE"
+const UPDATE = "UPDATE"
+const DELETE = "DELETE"
+
 function reducer(state, action) {
-  return state
+  switch (action.type) {
+    case CREATE:
+      return [action.data, ...state]
+    case UPDATE:
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      )
+    case DELETE:
+      return state.filter((item) => String(item.id) !== String(action.id))
+    default:
+      return state
+  }
 }
+
+const DiaryStateContext = createContext()
+const DiaryDispatchContext = createContext()
 
 function App() {
   const [data, dispatch] = useReducer(reducer, dummyData)
+
+  // 일기 추가
+  const onCreate = (createdDate, emtionId, content) => {
+    dispatch({
+      type: CREATE,
+      data: {
+        id: uuidv4(),
+        createdDate,
+        emtionId,
+        content,
+      },
+    })
+  }
+
+  // 일기 수정
+  const onUpdate = (id, createdDate, emtionId, content) => {
+    dispatch({
+      type: UPDATE,
+      data: {
+        id,
+        createdDate,
+        emtionId,
+        content,
+      },
+    })
+  }
+
+  // 일기 삭제
+  const onDelete = (id) => {
+    dispatch({
+      type: DELETE,
+      id,
+    })
+  }
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* URL Parameter(:id)활용하여 동적 라우팅 */}
-        <Route path="diary/:id" element={<Diary />} />
-        <Route path="new" element={<New />} />
-        <Route path="edit/:id" element={<Edit />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
+      <button
+        onClick={() => {
+          onCreate(new Date().getTime(), 1, "안농")
+        }}
+      >
+        일기 추가 테스트
+      </button>
+      <button
+        onClick={() => {
+          onUpdate(1, new Date().getTime(), 1, "안녕하십니까아")
+        }}
+      >
+        일기 수정 테스트
+      </button>
+      <button
+        onClick={() => {
+          onDelete(1)
+        }}
+      >
+        일기 삭제 테스트
+      </button>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* URL Parameter(:id)활용하여 동적 라우팅 */}
+            <Route path="diary/:id" element={<Diary />} />
+            <Route path="new" element={<New />} />
+            <Route path="edit/:id" element={<Edit />} />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   )
 }
