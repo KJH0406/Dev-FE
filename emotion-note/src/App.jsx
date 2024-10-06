c4c11e3490f8d49f12595c02ad32db718b9aed93
김장호 <rlawkdgh0406@naver.com>
2024-10-06 12:17:06 +0900

    feat: Link 설정 및 Navigate 생성

--- emotion-note/src/App.jsx
+++ emotion-note/src/App.jsx
@@ -1,24 +1,36 @@
 import "./App.css"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Link, useNavigate } from "react-router-dom"
 import Home from "./pages/Home"
 import Diary from "./pages/Diary"
 import New from "./pages/New"
 import Notfound from "./pages/Notfound"
 
 function App() {
+  const nav = useNavigate()
+  const onClickButton = () => {
+    nav("/new")
+  }
   return (
-    <Routes>
-      <Route path="/" element={<Home />}>
-        home
-      </Route>
-      <Route path="diary" element={<Diary />}>
-        diary
-      </Route>
-      <Route path="new" element={<New />}>
-        new
-      </Route>
-      <Route path="*" element={<Notfound />} />
-    </Routes>
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/diary">Diary</Link>
+        <Link to="/new">New</Link>
+      </div>
+      <button onClick={onClickButton}>새로운 일기 생성</button>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          home
+        </Route>
+        <Route path="diary" element={<Diary />}>
+          diary
+        </Route>
+        <Route path="new" element={<New />}>
+          new
+        </Route>
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
   )
 }
 
