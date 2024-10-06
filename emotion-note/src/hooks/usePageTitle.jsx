1a4f56ad02c6434be4dcab2832b300ee1c6cf759
김장호 <rlawkdgh0406@naver.com>
2024-10-07 01:22:34 +0900

    feat: 페이지 타이틀 변경 coustom hook 추가

---
+++ emotion-note/src/hooks/usePageTitle.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+
+// 페이지 타이틀 변경
+useEffect(
+  (title) => {
+    const $title = document.getElementsByTagName("title")[0]
+    $title.innerText = title
+  },
+  [title]
+)
+
+export default title
