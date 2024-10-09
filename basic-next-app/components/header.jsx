54393f52fab1fbd3851ca92e9ffdf5127379a8b0
김장호 <rlawkdgh0406@naver.com>
2024-10-09 16:11:38 +0900

    styles: css 모듈 사용하여 header 스타일링

--- basic-next-app/components/header.jsx
+++ basic-next-app/components/header.jsx
@@ -1,14 +1,15 @@
-import Logo from "@/assets/logo.png"
 import Link from "next/link"
+import Logo from "@/assets/logo.png"
+import "@/styles/header.css"
 
 const Header = () => {
   return (
-    <header>
-      <Link href="/">
+    <header className="header">
+      <Link className="logo" href="/">
         <img src={Logo.src} />
         Header
       </Link>
-      <nav>
+      <nav className="nav">
         <ul>
           <li>
             <Link href="/meals">Meals</Link>
