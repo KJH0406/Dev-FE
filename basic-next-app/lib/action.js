b917372ffc07cc60e5fddf220e6ebb5f1e5294d5
김장호 <rlawkdgh0406@naver.com>
2024-10-12 14:02:05 +0900

    feat: 음식 데이터 캐싱

--- basic-next-app/lib/action.js
+++ basic-next-app/lib/action.js
@@ -2,6 +2,11 @@
 
 import { redirect } from "next/navigation"
 import { saveMeal } from "./get-meals"
+import { revalidatePath } from "next/cache"
+
+const isInvalidText = (text) => {
+  return !text || text.trim() === ""
+}
 
 const shareMeal = async (formData) => {
   const meal = {
@@ -12,7 +17,25 @@ const shareMeal = async (formData) => {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   }
+
+  // BE 유효성 검사
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    return {
+      message: "Invalid input.",
+    }
+  }
+
   await saveMeal(meal)
+  revalidatePath("/meals")
   redirect("/meals")
 }
 
