97fa66f12b6606d5afca96f569d336ddd63da5b2
김장호 <rlawkdgh0406@naver.com>
2024-10-12 11:21:33 +0900

    feat: 음식 공유 페이지 구현

--- basic-next-app/app/meals/share/page.jsx
+++ basic-next-app/app/meals/share/page.jsx
@@ -1,9 +1,52 @@
-const SharePage = () => {
+import styles from "@/styles/share/share-meal-page.module.css"
+
+const ShareMealPage = () => {
   return (
-    <main>
-      <h1>SharePage</h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1>
+          당신의 <span className={styles.highlight}>좋아하는 음식</span>을
+          공유하세요
+        </h1>
+        <p>혹은 공유할만한 다른 음식을 알려주세요!</p>
+      </header>
+      <main className={styles.main}>
+        <form className={styles.form}>
+          <div className={styles.row}>
+            <p>
+              <label htmlFor="name">이름</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">이메일</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">제목</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">간단한 설명</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">조리 방법</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              required
+            ></textarea>
+          </p>
+          이미지 선택
+          <p className={styles.actions}>
+            <button type="submit">음식 공유하기</button>
+          </p>
+        </form>
+      </main>
+    </>
   )
 }
 
-export default SharePage
+export default ShareMealPage
